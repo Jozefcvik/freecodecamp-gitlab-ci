9f13d14fe9ba7cace23c2e5449f41c16c6a0727a
Jozef <82057450+Jozefcvik@users.noreply.github.com>
2025-07-15 20:53:04 +0200

    Hello from github

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GitLab CI Hurra :) 
+          Learn GitLab CI Hurra from github :) 
         </a>
       </header>
     </div>
